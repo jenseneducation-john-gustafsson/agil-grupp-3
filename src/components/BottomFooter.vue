b8cc6936ab33690be07c3826e96c7b9755824e8f
jenseneducation-john-gustafsson <john.gustafsson80@gmail.com>
2021-04-20 14:08:04 +0200

    hemsida klar

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,31 +1,57 @@
 <template>
-    <div class="footer-middle">
+  <div class="footer-middle">
     <div class="container">
       <div class="row">
         <div class="col-md-12 copy">
-          
           <div class="mb-3">
-      <div class="pt-4">
-       
-        <ul class="social-network social-circle">
+            <div class="pt-4">
+              <ul class="social-network social-circle">
                 <li>
-                  <a href="https://www.facebook.com" aria-label="facebook" target="_blank" class="sm-icons" title="Facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a
+                    href="https://www.facebook.com"
+                    aria-label="facebook"
+                    target="_blank"
+                    class="sm-icons"
+                    title="Facebook"
+                    ><i class="fa fa-facebook" aria-hidden="true"></i
+                  ></a>
                 </li>
                 <li>
-                  <a href="https://www.linkedin.com" aria-label="linkedin" target="_blank" class="sm-icons" title="Linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                  <a
+                    href="https://www.linkedin.com"
+                    aria-label="linkedin"
+                    target="_blank"
+                    class="sm-icons"
+                    title="Linkedin"
+                    ><i class="fa fa-linkedin" aria-hidden="true"></i
+                  ></a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com" aria-label="instagram" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  <a
+                    href="https://www.instagram.com"
+                    aria-label="instagram"
+                    target="_blank"
+                    class="icoInstagram"
+                    title="Instagram"
+                    ><i class="fa fa-instagram" aria-hidden="true"></i
+                  ></a>
                 </li>
                 <li>
-                    <a href="https://twitter.com" aria-label="twitter" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a
+                    href="https://twitter.com"
+                    aria-label="twitter"
+                    target="_blank"
+                    class="icoTwitter"
+                    title="Twitter"
+                    ><i class="fa fa-twitter" aria-hidden="true"></i
+                  ></a>
                 </li>
               </ul>
-        
-      </div>
-    </div>
+            </div>
+          </div>
           <p class="text-center">
-            &copy; Copyright 2021 - Bolagsnamn & Partners AB. All rights reserved.
+            &copy; Copyright 2021 - Bolagsnamn & Partners AB. All rights
+            reserved.
           </p>
         </div>
       </div>
@@ -34,9 +60,8 @@
 </template>
 <script>
 export default {
-    name: "bottomfooter",
-}
-
+  name: "bottomfooter",
+};
 </script>
 <style scoped>
 .footer-middle {
@@ -44,8 +69,7 @@ export default {
   position: fixed;
   width: 100%;
   bottom: 0;
-  }
-
+}
 
 .soc-media-content {
   display: flex;
@@ -64,14 +88,13 @@ ul.social-network li {
   display: inline;
   margin: 0 5px;
 }
-.social-network a.sm-icons:hover{
+.social-network a.sm-icons:hover {
   color: #44bcdd;
 }
 .mr-2 {
-    cursor: pointer;
+  cursor: pointer;
 }
-.text-center{
-    color: #ffff;
-    
+.text-center {
+  color: #ffff;
 }
 </style>
